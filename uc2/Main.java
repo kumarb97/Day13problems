0a24f1787e57fc1cb56e45d82c34df0ece1bf594
kumarb97 <kumarbamankar@gmail.com>
2022-03-02 11:41:29 +0530

    use case two added

---
+++ uc2/Main.java
@@ -0,0 +1,13 @@
+package uc2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Check maximum of three variables");
+		Float x = 1.5f, y = 2.5f, z = 0.65f;
+		TestMaximum.testMaximum(x, y, z);
+
+	}
+
+}
---
+++ uc2/TestMaximum.java
@@ -0,0 +1,33 @@
+/**UC2 : PROBLEM STATEMENT
+ * Find maximum of three float values
+ */
+package uc2;
+
+public class TestMaximum {
+
+	int x, y, z;
+
+	//constructor testmax
+		public TestMaximum(int x, int y, int z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+	//checking max of three floats values x,y,z
+		public static Float testMaximum(Float x, Float y, Float z) {
+			Float max = x;// Initializing x as greater value
+			if (y.compareTo(max) > 0)
+				max = y;// as of now y is greater in value
+
+			if (z.compareTo(max) > 0)
+				max = z;// now z is greater in value
+			printMax(x, y, z, max);
+			return max;
+
+		}
+
+		public static void printMax(Float x, Float y, Float z, Float max) {
+			System.out.printf("The maximum of Given Float values  %s , %s and %s is %s\n", x, y, z, max);
+		}
+	}
