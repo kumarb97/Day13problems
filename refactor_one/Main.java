6c9a0270509c1919680f540c7039f15754be9c7b
kumarb97 <kumarbamankar@gmail.com>
2022-03-02 11:43:52 +0530

    Refactor one added

---
+++ refactor_one/Main.java
@@ -0,0 +1,16 @@
+package refactor_one;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to check Maximum of three variables");
+		String x = "jack", y = "king", z = "queen";
+		Integer a = 25, b = 24, c = 19;
+		Float p = 1.23f, q = 2.2f, r = 0.2f;
+		TestMaximum.testMaximum(x, y, z);
+		TestMaximum.testMaximum(a, b, c);
+		TestMaximum.testMaximum(p, q, r);
+
+	}
+
+}
