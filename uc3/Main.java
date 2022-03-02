de8ef7b8616b712a3518c1de15ce329608887034
kumarb97 <kumarbamankar@gmail.com>
2022-03-02 11:42:04 +0530

    use case three added

---
+++ uc3/Main.java
@@ -0,0 +1,12 @@
+package uc3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Check maximum of three variables");
+		String x = "apple", y = "peach", z = "banana";
+		TestMaximum.testMaximum(x, y, z);
+
+	}
+
+}
---
+++ uc3/TestMaximum.java
@@ -0,0 +1,35 @@
+/**UC3 : PROBLEM STATEMENT
+ * Find maximum of three Strings values
+ *Eg :Apple,Peach,Bnanana
+ *
+ */
+package uc3;
+
+public class TestMaximum {
+
+	String x, y, z;
+
+	// constructor
+	public TestMaximum(String x, String y, String z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	// checking max of three integers
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;// initialising x as greater
+		if (y.compareTo(max) > 0)
+			max = y;// as of now y is greater
+
+		if (z.compareTo(max) > 0)
+			max = z;// now z is greater
+		printMax(x, y, z, max);
+		return max;
+
+	}
+
+	public static void printMax(String x, String y, String z, String max) {
+		System.out.printf("The maximum from string of %s , %s and %s is %s\n", x, y, z, max);
+	}
+}
