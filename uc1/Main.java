33a929f09836d5e44b52e0bcb45ed17f5ab4be43
kumarb97 <kumarbamankar@gmail.com>
2022-03-02 11:40:38 +0530

    use case one added

---
+++ uc1/Main.java
@@ -0,0 +1,16 @@
+package day13problems;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to check max of three variables");
+
+		Integer a = 25, b = 24, c = 19;
+
+		TestMaximum.testMaximum(a, b, c);
+
+
+	}
+
+}
